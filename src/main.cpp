c1514b07174390873f88adde14d82ab5fcb3accc
Troels Jessen <kairyuka@live.dk>
2019-03-18 21:09:23 +0100

    Kept window open and polling for events

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,13 @@
 
 void main() {
     sf::Window window(sf::VideoMode(800,600), "Hello World!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 }
