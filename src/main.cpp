7a0ff8cbfe18210fa77ca76c73792fbd079abb02
Troels Jessen <kairyuka@live.dk>
2019-03-19 20:49:11 +0100

    Implemented keypress background color change on space

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-void main() {
-    sf::RenderWindow window(sf::VideoMode(800,600), "Hello World!");
+void main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Hello World!");
 
     std::string fileName = "..\\graphics\\angery.png";
     sf::Texture texture;
@@ -16,14 +17,23 @@ void main() {
 
     while (window.isOpen())
     {
+        static sf::Color backgroundColor = sf::Color::Black;
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Space)
+                {
+                    backgroundColor = backgroundColor == sf::Color::Black ? sf::Color::Green : sf::Color::Black;
+                }
+            }
         }
 
-        window.clear(sf::Color::Black);
+        window.clear(backgroundColor);
 
         window.draw(sprite);
 
